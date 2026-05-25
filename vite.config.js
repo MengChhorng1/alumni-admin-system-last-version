616c5a5139b70bbcc6c0095dadf0bf627a57b966
MengChhorng1 <chhorng349@gmail.com>
2026-05-25 17:23:29 +0700

    fix: update base path in vite.config.js to point to the last version

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alumni-admin-system/',
+  base: '/alumni-admin-system-last-version/',
   server: {
     port: 5173,
   },
